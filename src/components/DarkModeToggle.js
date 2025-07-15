dce111771465b19bf26c9d3a3cbb45461a513708
TRANNGOCTAM <tran.ngoc.tam@uruca.jp>
2025-07-15 09:42:10 +0700

    remove all.

--- src/components/DarkModeToggle.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export default function DarkModeToggle({ darkMode, handleClick }) {
-    return (
-        <span onClick={handleClick} aria-label={darkMode}>{darkMode === 'light' ? '🌤️' : '🌒'}</span>
-    );
-};
